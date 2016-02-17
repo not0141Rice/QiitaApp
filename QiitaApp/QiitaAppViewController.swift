687bf84d3f34d21cb84f4b9c098356baca9f8d5a
0141Rice <ricefield1995@gmail.com>
2016-02-18 01:06:57 +0900

    Implement some protocols

--- QiitaApp/QiitaAppViewController.swift
+++ QiitaApp/QiitaAppViewController.swift
@@ -8,17 +8,29 @@
 
 import UIKit
 
-class QiitaAppViewController: UIViewController {
+class QiitaAppViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var qiitaEntities: [QiitaItems]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        qiitaEntities = QiitaItems.MR_findAll() as? [QiitaItems]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+        return qiitaEntities.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ListItem")!
+        cell.textLabel!.text = qiitaEntities[indexPath.row].item
+        return cell
     }
 
 
