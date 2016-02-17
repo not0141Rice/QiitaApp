d8474804f32de3614566053b6179da60e2721dc5
0141Rice <ricefield1995@gmail.com>
2016-02-17 21:20:34 +0900

    pod install MagicalRecord

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.3'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'QiitaApp' do
+    pod 'MagicalRecord'
+end
+
+target 'QiitaAppTests' do
+
+end
+
+target 'QiitaAppUITests' do
+
+end
+
---
+++ QiitaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QiitaApp
+//
+//  Created by @0141Rice on 2016/02/17.
+//  Copyright © 2016年 0141Rice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
