030f9db0b63a542624998c1f4c352a383530fad9
0141Rice <ricefield1995@gmail.com>
2016-02-18 00:16:25 +0900

    create main view controller

---
+++ QiitaApp/ListItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListItemTableViewCell.swift
+//  QiitaApp
+//
+//  Created by @0141Rice on 2016/02/18.
+//  Copyright © 2016年 0141Rice. All rights reserved.
+//
+
+import UIKit
+
+class ListItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- QiitaApp/QiitaAppViewController.swift
+++ QiitaApp/QiitaAppViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class QiitaAppViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
